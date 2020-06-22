2430059128f68bc68e1d604f50a53dc01c9ddce6
Andrey Tkachuk <45736309+AndreyTkachuk@users.noreply.github.com>
2020-06-22 22:26:40 +0300

    SpringMarket

---
+++ src/main/java/com/company/config/WebConfiguration.java
@@ -0,0 +1,31 @@
+package com.company.config;
+
+import org.springframework.context.MessageSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+ 
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+ 
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        
+        messageSource.setBasename("classpath:validation");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+    
+    
+    @Bean
+    public CommonsMultipartResolver  multipartResolver() {
+    	CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+    	return multipartResolver;
+    	
+    }
+  
+}
